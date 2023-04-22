2e94e57acfd155c2f36f639a33f35c5193b31e93
Naveen Joy <naveenjoy18@gmail.com>
2023-04-23 00:18:42 +0530

    blog, footer completed, build_v1

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,7 +7,12 @@ import {
   Rectangle24,
   Rectangle25,
   Rectangle26,
-} from "./imports";
+} from "./imports2";
+// import Rectangle22 from "../../assets/Rectangle22.png";
+// import Rectangle23 from "../../assets/Rectangle23.png";
+// import Rectangle24 from "../../assets/Rectangle24.png";
+// import Rectangle25 from "../../assets/Rectangle25.png";
+// import Rectangle26 from "../../assets/Rectangle26.png";
 
 const Blog = () => {
   return (
--- src/containers/blog/imports.js
+++
@@ -1,7 +0,0 @@
-import Rectangle22 from "../../assets/Rectangle22.png";
-import Rectangle23 from "../../assets/Rectangle23.png";
-import Rectangle24 from "../../assets/Rectangle24.png";
-import Rectangle25 from "../../assets/Rectangle25.png";
-import Rectangle26 from "../../assets/Rectangle26.png";
-
-export { Rectangle22, Rectangle23, Rectangle24, Rectangle25, Rectangle26 };
